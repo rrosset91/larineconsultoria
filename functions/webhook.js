b04841211e85b57c01774c2e4a4626819c85e913
Roger Rosset <rrosset91@gmail.com>
2025-01-03 21:50:12 +0000

    923312

--- functions/webhook.js
+++ functions/webhook.js
@@ -5,8 +5,8 @@ export async function onRequestPost(context) {
 	const payload = await request.text();
   
 	try {
-	  const stripe = require("stripe")('sk_test_51QdDLzBmLhzPvPbKglzZmKrVcOuW7d4NgyOKngDkB0FptyY1yTC10rpgqm1lTXZ1IVJyVwCYVyV3JculRxSWSvA100Tqt65jWb');
-	  const event = stripe.webhooks.constructEvent(payload, sig, 'sk_test_51QdDLzBmLhzPvPbKglzZmKrVcOuW7d4NgyOKngDkB0FptyY1yTC10rpgqm1lTXZ1IVJyVwCYVyV3JculRxSWSvA100Tqt65jWb');
+	  const stripe = require("stripe")(env.STRIPE_SECRET_KEY);
+	  const event = stripe.webhooks.constructEvent(payload, sig, env.STRIPE_WEBHOOK_SECRET);
   
 	  if (event.type === "payment_intent.succeeded") {
 		console.log("Pagamento bem-sucedido:", event.data.object);

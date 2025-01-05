434f7d73fe5498dcbccc53647e9b3d7adc97a915
Roger Rosset <rrosset91@gmail.com>
2025-01-05 16:21:54 +0000

    undo

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -3,40 +3,8 @@ export async function onRequestPost(context) {
   
 	try {
 	  const stripe = require("stripe")(env.STRIPE_SECRET_KEY);
-	  // Obter os dados enviados do frontend
-	  const { amount, turnstileToken } = await request.json();
-
-	  // Verificar se o token do Turnstile foi enviado
-	  if (!turnstileToken) {
-		return new Response(
-		  JSON.stringify({ error: "Turnstile token is missing" }),
-		  { status: 400 }
-		);
-	  }
-  
-	  // Validar o token do Turnstile com a API da Cloudflare
-	  const turnstileResponse = await fetch(
-		"https://challenges.cloudflare.com/turnstile/v0/siteverify",
-		{
-		  method: "POST",
-		  headers: { "Content-Type": "application/json" },
-		  body: JSON.stringify({
-			secret: env.TURNSTILE_SECRET_KEY, // Chave secreta configurada no wrangler.toml
-			response: turnstileToken,
-		  }),
-		}
-	  );
-  
-	  const turnstileResult = await turnstileResponse.json();
-  
-	  // Verificar o resultado da validação do Turnstile
-	  if (!turnstileResult.success) {
-		return new Response(
-		  JSON.stringify({ error: "Failed Turnstile validation" }),
-		  { status: 403 }
-		);
-	  }
-  
+	  const { amount } = await request.json();
+	  
   
 	  if (!amount || amount <= 0) {
 		return new Response(
